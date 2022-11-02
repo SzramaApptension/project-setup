a9801620f3cdeffdf5dda1f6087d38c8fc3bbc1f
Rafal Szrama <rszrama@apptension.com>
2022-11-02 09:46:49 +0100

    option4

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,13 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    viewportWidth: 360,
+    viewportHeight: 720,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+      baseUrl: 'http://localhost:1234'
+    }
   },
+  env: {
+    url: 'https://app.qa.hoh-cloud.apptoku.com/'
+  }
 });
